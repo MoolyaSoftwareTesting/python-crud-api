aac15d465dcfbc099775c1a781bc02e00971ed4b
Raghavendra <raghavendra8076@gmail.com>
2023-09-21 15:25:03 +0530

    V1.0.1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
 
+import json
+import sys
+
+file_name = sys.argv[1]
+file_open = open(f'{file_name}')
+
+data = json.load(file_open)
+
+string = ""
+
+data_2 = []
+
+for i in data['name']:
+    for j in dict.values(i):
+        string = string + j
+
+
+print(string)
