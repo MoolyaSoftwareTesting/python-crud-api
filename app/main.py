5c03d599b48819fee1bcb934eaaf50c44944c4ab
Raghavendra <raghavendra8076@gmail.com>
2023-09-22 15:08:48 +0530

    V2.0.4

--- app/main.py
+++ app/main.py
@@ -3,11 +3,10 @@ file_open = open("data.json")
 data = json.load(file_open)
 string = ""
 
-
 def load_json(data):
     global string
     string = data['name'][0]['name'] + " " + data['name'][1]['name']
     print(string)
 
-# Checking if my commit works V2.0.3
+# Checking if my commit works V2.0.4
 load_json(data=data)
