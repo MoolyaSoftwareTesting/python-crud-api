33a8fe1b072a5db480950194a078110de3f910ca
raghavmoolya <144980919+raghavmoolya@users.noreply.github.com>
2023-09-22 15:13:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,9 @@ import json
 file_open = open("data.json")
 data = json.load(file_open)
 string = ""
-
 def load_json(data):
     global string
     string = data['name'][0]['name'] + " " + data['name'][1]['name']
     print(string)
-
 # Checking if my commit works V2.0.4
 load_json(data=data)
