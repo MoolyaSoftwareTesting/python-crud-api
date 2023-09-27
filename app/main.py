d14918902176b558d52d8624e964c447fdfd5316
Raghavendra <raghavendra8076@gmail.com>
2023-09-27 12:50:59 +0530

    V2.0.5

--- app/main.py
+++ app/main.py
@@ -6,5 +6,6 @@ def load_json(data):
     string = data['name'][0]['name'] + " " + data['name'][1]['name']
     return string
 
+
 # Checking if my commit works V2.0.4
 print(load_json(data="data.json"))
