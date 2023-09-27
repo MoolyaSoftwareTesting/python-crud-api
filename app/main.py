6cf724546c133c21e37fa58d024741ff8c15844f
Raghavendra <raghavendra8076@gmail.com>
2023-09-27 14:37:41 +0530

    V2.0.6

--- app/main.py
+++ app/main.py
@@ -5,6 +5,5 @@ def load_json(data):
     json_data = json.load(file_open)
     string = json_data['name'][0]['name'] + " " + json_data['name'][1]['name']
     return string
-
 #2.0.6
 print(load_json("data.json"))
