dacd6fcee85105461ad360b1ca24e5d8e92a6a94
Raghavendra <raghavendra8076@gmail.com>
2023-10-02 13:21:22 +0530

    'update_json' function

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 import json
 
+
 def load_json(data):
-    global return_data
     file_open = open(data)
     json_data = json.load(file_open)
-    return json_data[0]
+    update_json(json_data["data"][0])
+    # return json_data["data"][0]
 
 
-def update_json(new_data, file_name="data.json"):
-    with open("data.json", 'r+') as file:
+def update_json(new_data, filename='data.json'):
+    with open(filename, 'r+') as file:
         file_data = json.load(file)
-        file_data.append(new_data)
+        file_data["data"].append(new_data)
         file.seek(0)
         json.dump(file_data, file, indent=4)
+        string = "Done Right"
+        return string
 
 
-update_json(load_json("data.json"))
-# V2.0.10
-#print(load_json("data.json"))
+load_json("data.json")
