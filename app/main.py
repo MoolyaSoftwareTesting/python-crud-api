30788111f49445c8410b12cf65638ee93613ebf2
Raghavendra <raghavendra8076@gmail.com>
2023-10-04 15:06:49 +0530

    V2.0.12

--- app/main.py
+++ app/main.py
@@ -1,19 +1,28 @@
 import json
 
+NEW_DATA = {
+    "firstname": "DS",
+    "lastname": "S",
+    "mobile": "99999999",
+    "email": "email@GMAIL.com"
+}
+
 
 def load_json(data):
-    file_open = open(data)
-    json_data = json.load(file_open)
-    update_json(json_data["data"][0])
+    with open(data, "r") as file:
+        json_data = json.load(file)
+    return json_data
 
 
-def update_json(new_data, filename='data.json'):
-    with open(filename, 'r+') as file:
-        file_data = json.load(file)
-        file_data["data"].append(new_data)
-        file.seek(0)
-        json.dump(file_data, file, indent=4)
+def update_json(n_data, filename):
+    data = load_json(filename)
+    data['data'].append(n_data)
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+    with open(filename, "r") as updated_file:
+        json_updated_file = json.load(updated_file)
+        return json_updated_file
 
 
-# V2.0.11
-load_json("data.json")
+# V2.0.12
+print(update_json(NEW_DATA, "data2.json"))
