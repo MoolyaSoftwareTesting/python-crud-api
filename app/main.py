92c50c73083b874b8c0d412553b58abeb1588fa4
Raghavendra <raghavendra8076@gmail.com>
2023-10-05 15:02:03 +0530

    V2.1

--- app/main.py
+++ app/main.py
@@ -9,30 +9,46 @@ NEW_DATA = {
 
 
 def load_json(data):
-    with open(data, "r") as file:
-        json_data = json.load(file)
-    return json_data
+    try:
+        with open(data, "r") as file:
+            json_data = json.load(file)
+        return json_data
+    except FileNotFoundError:
+        return 1
+    except json.decoder.JSONDecodeError:
+        return 2
 
 
 def update_json(n_data, filename):
     data = load_json(filename)
-    data['data'].append(n_data)
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=4)
-    return load_json(filename)
+    if data == 1:
+        return "Invalid JSON"
+    elif data == 2:
+        return "It is not a JSON file"
+    else:
+        data["data"].append(n_data)
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=4)
+        return load_json(filename)
 
 
 def load_txt(n_data, filename):
-    with open(filename) as f:
-        lines = f.readlines()
-    with open(filename, "a") as update_txt:
-        update_txt.write(f"firstname: {n_data['firstname']}\n")
-        update_txt.write(f"lastname: {n_data['lastname']}\n")
-        update_txt.write(f"mobile: {n_data['mobile']}\n")
-        update_txt.write(f"email: {n_data['email']}\n \n")
-    return lines
+    try:
+        with open(filename) as f:
+            lines = f.readlines()
+        with open(filename, "a") as update_txt:
+            update_txt.write(f"firstname: {n_data['firstname']}\n")
+            update_txt.write(f"lastname: {n_data['lastname']}\n")
+            update_txt.write(f"mobile: {n_data['mobile']}\n")
+            update_txt.write(f"email: {n_data['email']}\n \n")
+        return lines
+    except FileNotFoundError:
+        return "Invalid txt file"
 
 
-# V2.0.13
-print(update_json(NEW_DATA, "data.json"))
-print(load_txt(NEW_DATA, "text.txt"))
+
+
+
+# V2.1
+# print(update_json(NEW_DATA, "data.json"))
+print(load_txt(NEW_DATA, "text.json"))
